274c4e5c524058141d736a0be0dc7478713366db
okristiansen <oliver.hansen.kristiansen@no.experis.com>
2026-08-31 15:19:26 +0200

    Feat: Added recipebookrepository and irecipebookrepository

---
+++ backend/Cookbook-app/Cookbook-app/Repositories/RecipeBookRepository.cs
@@ -0,0 +1,36 @@
+﻿using Cookbook_app.Data;
+using Cookbook_app.Models;
+
+namespace Cookbook_app.Repositories;
+
+public class RecipeBookRepository : IRecipeBookRepository
+{
+    
+    private readonly CookbookDbContext _context;
+
+    public RecipeBookRepository(CookbookDbContext context)
+    {
+        _context = context;
+    }
+    
+    
+    public Task<RecipeBook> GetRecipeBookById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task AddRecipeBook(RecipeBook recipeBook)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteRecipeBook(RecipeBook recipeBook)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateRecipeBook(RecipeBook recipeBook)
+    {
+        throw new NotImplementedException();
+    }
+}
