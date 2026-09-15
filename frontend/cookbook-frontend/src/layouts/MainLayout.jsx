06b25c3df9b716ffcd0686d72bed7cdd262ae2dc
DAVNGU99 <david.h.nguyen1999@gmail.com>
2026-09-15 12:57:58 +0200

    added typescript for the UI library

--- frontend/cookbook-frontend/src/layouts/MainLayout.jsx
+++ frontend/cookbook-frontend/src/layouts/MainLayout.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import { UserPlus } from "lucide-react";
 
 function MainLayout() {
   return (
-    <div>
+    <div className="bg-blue-200 min-h-dvh flex center-items justify-center">
       <header>
         <Navbar />
+        <UserPlus />
       </header>
 
       <main>
--- frontend/cookbook-frontend/vite.config.js
+++ frontend/cookbook-frontend/vite.config.js
@@ -2,6 +2,7 @@ import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath, URL } from "node:url";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,4 +11,10 @@ export default defineConfig({
     react(),
     babel({ presets: [reactCompilerPreset()] }),
   ],
+
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
 });
