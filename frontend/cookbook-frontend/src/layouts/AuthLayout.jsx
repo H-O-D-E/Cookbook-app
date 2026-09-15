f03a70a072b27f919e9a90d6b05182d8e15e40db
DAVNGU99 <david.h.nguyen1999@gmail.com>
2026-09-15 11:30:59 +0200

    feat: setting up the frontend skeleton for a good dev environment. Installing react-router, react-toast, tailwindcss, tanstack-query

---
+++ frontend/cookbook-frontend/src/layouts/AuthLayout.jsx
@@ -0,0 +1,5 @@
+function AuthLayout() {
+  return <div>Auth layout</div>;
+}
+
+export default AuthLayout;
---
+++ frontend/cookbook-frontend/src/layouts/MainLayout.jsx
@@ -0,0 +1,23 @@
+import { Outlet } from "react-router";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+function MainLayout() {
+  return (
+    <div>
+      <header>
+        <Navbar />
+      </header>
+
+      <main>
+        <Outlet />
+      </main>
+
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
+}
+
+export default MainLayout;
