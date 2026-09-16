eb95bc9ee6fa3acddc68e2687746930438f342dc
DAVNGU99 <david.h.nguyen1999@gmail.com>
2026-09-16 14:23:06 +0200

    change to explcit allow localhost:5173

--- backend/Cookbook-app/Cookbook-app/Program.cs
+++ backend/Cookbook-app/Cookbook-app/Program.cs
@@ -52,7 +52,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("ReactFrontend", policy =>
     {
-        policy.WithOrigins(allowedFrontendOrigins)
+        policy.WithOrigins("http://localhost:5173")
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
