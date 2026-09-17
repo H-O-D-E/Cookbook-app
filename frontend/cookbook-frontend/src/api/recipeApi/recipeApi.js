1c4fc8e060b2b4c2a4f18e06a6f48685fd617b7b
okristiansen <oliver.hansen.kristiansen@no.experis.com>
2026-09-17 11:38:25 +0200

    Feat: Added initial recipeApi.js implementation

---
+++ frontend/cookbook-frontend/src/api/recipeApi/recipeApi.js
@@ -0,0 +1,47 @@
+import {apiFetch} from '../apiFetch';
+
+export async function getRecipe(recipeId) {
+    const response = await apiFetch(`/api/recipes/${recipeId}`);
+
+    if (!response.ok) {
+        throw new Error("Unable to find recipe :(");
+    }
+
+    return response.json();
+}
+
+export async function createRecipe({name, description, ingredients, instructions, recipeBookId}) {
+    const response = await apiFetch("api/recipes", {
+        method: "POST",
+        body: JSON.stringify({ name, description, ingredients, instructions, recipebookId: recipeBookId }),
+    });
+
+    if (!response.ok) {
+        throw new Error("Error when creating recipe");
+    }
+
+    return response.json();
+}
+
+export async function updateRecipe(recipeId, {name, description, ingredients, instructions}) {
+    const response = await apiFetch(`/api/recipes/${recipeId}`, {
+        method: "PUT",
+        body: JSON.stringify({ name, description, ingredients, instructions }),
+    });
+
+    if (!response.ok) {
+        throw new Error("Failed to update recipe");
+    }
+    return response.json();
+}
+
+export async function deleteRecipe(recipeId) {
+    const response = await apiFetch(`/api/recipes/${recipeId}`, {
+        method: "DELETE"
+    });
+
+    if (!response.ok) {
+        throw new Error("Failed to delete recipe");
+    }
+}
+
