bbe8e632d93afec8a8eefb04b7b28bb74e8bf68c
DAVNGU99 <david.h.nguyen1999@gmail.com>
2026-09-17 10:09:10 +0200

    feat: changed the authlayout page and added a picture

--- frontend/cookbook-frontend/src/layouts/AuthLayout.jsx
+++ frontend/cookbook-frontend/src/layouts/AuthLayout.jsx
@@ -6,13 +6,27 @@ function AuthLayout() {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <div className="flex min-h-dvh items-center justify-center  bg-neo-grid">
+    <div className="min-h-dvh bg-neo-grid grid grid-cols-1 lg:grid-cols-2">
+      <div className="flex flex-col items-center justify-center p-8">
+        <h1 className="text-6xl font-bold mb-6 text-black">I'm Cooked</h1>
+        <h2 className="text-black text-2xl">
+          Create your own cookbook and share it with the world!
+        </h2>
+        <img
+          src="/LoginPicture.svg"
+          alt=""
+          className="hidden lg:block w-full max-w-2xl h-auto"
+        />
+      </div>
+      <div className="flex items-center justify-center px-6 pb-8 lg:mr-30 lg:p-0">
+        <Outlet />
+      </div>
+
       <div className="fixed top-5 right-5 m-2">
         <button className="btn btn-circle btn-xl">
           <ThemeToggler theme={theme} toggleTheme={toggleTheme} />
         </button>
       </div>
-      <Outlet />
     </div>
   );
 }
