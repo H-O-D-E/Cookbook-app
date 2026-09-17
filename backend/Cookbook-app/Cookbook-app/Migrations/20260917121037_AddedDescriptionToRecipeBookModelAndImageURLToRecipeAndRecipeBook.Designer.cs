fd554018892bb34df121232cdd4ab44644ee9ae6
Håkon Lervåg <hakon.lervag@no.experis.com>
2026-09-17 15:57:21 +0200

    Added Image Urls to Recipes and Recipebooks + description. Added requirements to username, email, and password for user registration

--- backend/Cookbook-app/Cookbook-app/Migrations/20260917121037_AddedDescriptionToRecipeBookModelAndImageURLToRecipeAndRecipeBook.Designer.cs
+++ backend/Cookbook-app/Cookbook-app/Migrations/20260917121037_AddedDescriptionToRecipeBookModelAndImageURLToRecipeAndRecipeBook.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Cookbook_app.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Cookbook_app.Migrations
 {
     [DbContext(typeof(CookbookDbContext))]
-    partial class CookbookDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260917121037_AddedDescriptionToRecipeBookModelAndImageURLToRecipeAndRecipeBook")]
+    partial class AddedDescriptionToRecipeBookModelAndImageURLToRecipeAndRecipeBook
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
