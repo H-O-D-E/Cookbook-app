7d59c820ad56668a4b6ec1de416532aab31c4146
okristiansen <oliver.hansen.kristiansen@no.experis.com>
2026-09-17 11:04:18 +0200

    Feature: Initial implementation of recipeBookApi.js

---
+++ frontend/cookbook-frontend/src/api/recipeBookApi/recipeBookApi.js
@@ -0,0 +1,45 @@
+import {apiFetch} from '../apiFetch';c
+
+export async function getRecipeBook() {
+    const response = await apiFetch('/api/recipebooks/$1{id}');
+
+    if (!response.ok) {
+        throw new Error("Unable to find recipe book")
+    }
+
+    return response.json();
+}
+
+export async function createRecipeBook(recipeBookName) {
+    const response = await apiFetch("api/recipebooks", {
+        method: "POST",
+        body: JSON.stringify({ recipeBookName }),
+    })
+
+    if (!response.ok) {
+        throw new Error("Unable to create recipe book")
+    }
+    return response.json();
+
+}
+
+export async function updateRecipeBook(id, name) {
+    const response = await apiFetch(`/api/recipebooks/${id}`, {
+        method: "PUT",
+        body: JSON.stringify({ name }),
+    });
+
+    if (!response.ok) {
+        throw new Error("Unable to update recipe book");
+    }
+}
+
+export async function deleteRecipeBook(id) {
+    const response = await apiFetch(`/api/recipebooks/${id}`, {
+        method: "DELETE",
+    });
+
+    if (!response.ok) {
+        throw new Error("Unable to delete recipe book");
+    }
+}
